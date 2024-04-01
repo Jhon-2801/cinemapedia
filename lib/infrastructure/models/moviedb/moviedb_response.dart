eff9a9b9f88d9f95276a62040791c09ba4fee040
John Alex <jangulovides@gmail.com>
2024-04-01 10:45:35 -0500

    infrastructure

---
+++ lib/infrastructure/models/moviedb/moviedb_response.dart
@@ -0,0 +1,42 @@
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieDbResponse {
+  final Dates? dates;
+  final int page;
+  final List<MovieModel> results;
+  final int totalPages;
+  final int totalResults;
+
+  MovieDbResponse({
+    required this.dates,
+    required this.page,
+    required this.results,
+    required this.totalPages,
+    required this.totalResults,
+  });
+
+  factory MovieDbResponse.fromJson(Map<String, dynamic> json) =>
+      MovieDbResponse(
+        dates: json['dates'] ? Dates.fromJson(json["dates"]) : null,
+        page: json["page"],
+        results: List<MovieModel>.from(
+            json["results"].map((x) => MovieModel.fromJson(x))),
+        totalPages: json["total_pages"],
+        totalResults: json["total_results"],
+      );
+}
+
+class Dates {
+  final DateTime maximum;
+  final DateTime minimum;
+
+  Dates({
+    required this.maximum,
+    required this.minimum,
+  });
+
+  factory Dates.fromJson(Map<String, dynamic> json) => Dates(
+        maximum: DateTime.parse(json["maximum"]),
+        minimum: DateTime.parse(json["minimum"]),
+      );
+}
