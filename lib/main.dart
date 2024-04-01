bdbda463bb070272a9a57a52f8b663c70a6a2da9
John Alex <jangulovides@gmail.com>
2024-03-31 20:58:57 -0500

    environment variables configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:cinemapedia/config/router/app_router.dart';
+import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MainApp());
 }
 
@@ -10,14 +13,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
     );
   }
 }
